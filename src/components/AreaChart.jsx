d269790833f6a5ee2d38c30c7805fca115748476
Stéphane Bisinger <stephane@sbisinger.ch>
2022-09-28 19:07:42 +0200

    feat: Allow choosing the bottom electricity source

--- src/components/AreaChart.jsx
+++ src/components/AreaChart.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import { Radio, Select, Space } from "antd";
+const { Option } = Select;
+import styles from "./AreaChart.module.css";
 
-export function formatMonthlyEnergyMix(scenario) {
-  const sum = (a, b) => a + b;
-  return scenario.data.energySources
-    .filter(
-      (sourceName) =>
-        sourceName.startsWith("Electricity") && !sourceName.endsWith("|Total")
-    )
-    .map((sourceName) => {
-      const source = scenario.data[sourceName];
-      return {
-        name: sourceName.replace("Electricity|", ""),
-        data: source.monthlyValues,
-      };
-    })
-    .filter((series) => series.data.some((value) => value != 0))
-    .sort((a, b) => a.data.reduce(sum) - b.data.reduce(sum));
-}
-const AreaChart = ({ series, stackingMode, type }) => {
-  const getChartOptions = (stackingMode, chartType) => ({
+function getChartOptions(series, stackingMode, chartType) {
+  return {
     chart: { type: chartType },
     title: { text: "" },
     xAxis: {
@@ -57,20 +43,126 @@ const AreaChart = ({ series, stackingMode, type }) => {
       },
     },
     series: series,
+  };
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function compareLargestContributor(a, b) {
+  return a.data.reduce(sum) - b.data.reduce(sum);
+}
+
+export function formatMonthlyEnergyMix(scenario) {
+  return scenario.data.energySources
+    .filter(
+      (sourceName) =>
+        sourceName.startsWith("Electricity") && !sourceName.endsWith("|Total")
+    )
+    .map((sourceName) => {
+      const source = scenario.data[sourceName];
+      return {
+        name: sourceName.replace("Electricity|", ""),
+        data: source.monthlyValues,
+      };
+    })
+    .filter((series) => series.data.some((value) => value != 0))
+    .map((serie, index) => ({ ...serie, colorIndex: index }))
+    .sort(compareLargestContributor);
+}
+
+function calculateOrderOptions(series) {
+  const options = series.map(({ name }) => [name, name]);
+  return [...options].reverse();
+}
+
+function order(series, selectedOrder) {
+  return [...series].sort((a, b) => {
+    if (a.name == selectedOrder) {
+      return 1;
+    }
+    if (b.name == selectedOrder) {
+      return -1;
+    }
+    return compareLargestContributor(a, b);
   });
+}
+
+const AreaChart = ({ series }) => {
+  const [stackingMode, setStackingMode] = useState("normal");
+  const [chartType, setChartType] = useState("area");
+
+  const onChangeStackingMode = useCallback(({ target }) => {
+    setStackingMode(target.value);
+  }, []);
+  const onChangeChartType = useCallback(({ target }) => {
+    setChartType(target.value);
+  }, []);
+
+  const [orderOptions, setOrderOptions] = useState(
+    calculateOrderOptions(series)
+  );
+
+  useEffect(() => {
+    setOrderOptions(calculateOrderOptions(series));
+  }, [series]);
+
+  const [selectedOrder, setSelectedOrder] = useState(orderOptions[0][0]);
+
+  const [orderedSeries, setOrderedSeries] = useState(
+    order(series, selectedOrder)
+  );
+
+  useEffect(() => {
+    setOrderedSeries(order(series, selectedOrder));
+  }, [series, selectedOrder]);
+
   const [chartOptions, setChartOptions] = useState(
-    getChartOptions(stackingMode, type)
+    getChartOptions(orderedSeries, stackingMode, chartType)
   );
   useEffect(() => {
-    setChartOptions(getChartOptions(stackingMode, type));
-  }, [series, stackingMode, type]);
+    setChartOptions(getChartOptions(orderedSeries, stackingMode, chartType));
+  }, [orderedSeries, stackingMode, chartType]);
+
   return (
-    <HighchartsReact
-      allowChartUpdate={true}
-      immutable={false}
-      highcharts={Highcharts}
-      options={chartOptions}
-    />
+    <section className={styles.container}>
+      <Space size="small">
+        <Radio.Group
+          size="medium"
+          value={stackingMode}
+          onChange={onChangeStackingMode}
+        >
+          <Radio.Button value="normal">Absolute</Radio.Button>
+          <Radio.Button value="percent">Relative (%)</Radio.Button>
+        </Radio.Group>
+        <Radio.Group
+          size="medium"
+          value={chartType}
+          onChange={onChangeChartType}
+        >
+          <Radio.Button value="area">Area</Radio.Button>
+          <Radio.Button value="column">Bar</Radio.Button>
+        </Radio.Group>
+        <Select
+          defaultValue={orderOptions[0][1]}
+          value={selectedOrder}
+          onChange={setSelectedOrder}
+        >
+          {orderOptions.map(([value, label]) => (
+            <Option key={value} value={value}>
+              {label}
+            </Option>
+          ))}
+        </Select>
+      </Space>
+      <HighchartsReact
+        allowChartUpdate={true}
+        immutable={false}
+        highcharts={Highcharts}
+        options={chartOptions}
+      />
+    </section>
   );
 };
 
--- src/pages/detail/DetailPage.jsx
+++ src/pages/detail/DetailPage.jsx
@@ -35,18 +35,10 @@ const timesOfYear = [
 const DetailPage = () => {
   const [timeOfYear, setTimeOfYear] = useState("yearValue");
   const [scenarioData, setScenarioData] = useState(null);
-  const [stackingMode, setStackingMode] = useState("normal");
-  const [chartType, setChartType] = useState("area");
   const { id } = useParams();
 
   const onChangeRadio = useCallback(({ target }) => {
     setTimeOfYear(target.value);
-  }, []);
-  const onChangeStackingMode = useCallback(({ target }) => {
-    setStackingMode(target.value);
-  }, []);
-  const onChangeChartType = useCallback(({ target }) => {
-    setChartType(target.value);
   }, []);
 
   useEffect(() => {
@@ -118,30 +110,7 @@ const DetailPage = () => {
           <Typography.Title level={2}>
             Electricity mix over a year
           </Typography.Title>
-          <Space size="small">
-            <Radio.Group
-              size="medium"
-              value={stackingMode}
-              onChange={onChangeStackingMode}
-            >
-              <Radio.Button value="normal">Absolute</Radio.Button>
-              <Radio.Button value="percent">Relative (%)</Radio.Button>
-            </Radio.Group>
-            <Radio.Group
-              size="medium"
-              value={chartType}
-              onChange={onChangeChartType}
-            >
-              <Radio.Button value="area">Area</Radio.Button>
-              <Radio.Button value="column">Bar</Radio.Button>
-            </Radio.Group>
-          </Space>
-          <AreaChart
-            series={formatMonthlyEnergyMix(scenarioData)}
-            stackingMode={stackingMode}
-            type={chartType}
-            id={id}
-          />
+          <AreaChart series={formatMonthlyEnergyMix(scenarioData)} id={id} />
         </Space>
       </div>
     </ApplicationWrapper>
