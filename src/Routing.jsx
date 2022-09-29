b944e4759334cf19c088ebd93d6944660799c2dc
Stéphane Bisinger <stephane@sbisinger.ch>
2022-09-29 14:33:43 +0200

    feat: Add back static comparison with 7 scenarios

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -5,15 +5,17 @@ import DetailPage from "./pages/detail/DetailPage";
 import { routes } from "./routes";
 import NotFoundPage from "./pages/notfound/NotFoundPage";
 import ComparisonPage from "./pages/comparison/ComparisonPage";
+import ComparisonDemoPage from "./pages/comparison-demo/ComparisonDemoPage";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.home} element={<HomePage />} />
-        <Route path={routes.scenario} element={<DetailPage />}/>
-        <Route path={routes.comparison()} element={<ComparisonPage />}/>
-        <Route path='*' element={<NotFoundPage/>}/>
+        <Route path={routes.scenario} element={<DetailPage />} />
+        <Route path={routes.comparison()} element={<ComparisonPage />} />
+        <Route path={routes.comparison_demo} element={<ComparisonDemoPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
