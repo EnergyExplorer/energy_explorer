1619dd96de0585a703afb1a6f51b69c0a52921bd
kevnflyn <rwestanton@gmail.com>
2022-09-29 09:19:22 +0100

    feat: weight icon color inherited from parent state

--- src/components/WeightIcon.jsx
+++ src/components/WeightIcon.jsx
@@ -8,9 +8,9 @@ const WeightIcon = props => (
       <SvgIcon>
         <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
  width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000"
-  preserveAspectRatio="xMidYMid meet">
+  preserveAspectRatio="xMidYMid meet" fill="currentColor">
           <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-          fill="#000000" stroke="none">
+          stroke="none">
           <path d="M2488 5099 c-23 -12 -46 -35 -58 -59 -20 -38 -20 -57 -20 -922 l0
           -883 -1153 -319 c-633 -176 -1164 -325 -1179 -332 -14 -8 -36 -27 -48 -44 -69
           -97 0 -232 117 -232 29 0 161 32 336 81 159 45 292 81 297 81 5 0 -167 -294
