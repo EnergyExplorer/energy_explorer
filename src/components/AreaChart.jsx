678c13cf7b3c1875cd18b87104ff57910ac10299
kevnflyn <rwestanton@gmail.com>
2022-10-01 20:43:24 +0100

    refactor: create separate component for area chart controls

--- src/components/AreaChart.jsx
+++ src/components/AreaChart.jsx
@@ -127,35 +127,11 @@ const AreaChart = ({ series }) => {
 
   return (
     <section className={styles.container}>
-      <Space size="small">
-        <Radio.Group
-          size="medium"
-          value={stackingMode}
-          onChange={onChangeStackingMode}
-        >
-          <Radio.Button value="normal">Absolute</Radio.Button>
-          <Radio.Button value="percent">Relative (%)</Radio.Button>
-        </Radio.Group>
-        <Radio.Group
-          size="medium"
-          value={chartType}
-          onChange={onChangeChartType}
-        >
-          <Radio.Button value="area">Area</Radio.Button>
-          <Radio.Button value="column">Bar</Radio.Button>
-        </Radio.Group>
-        <Select
-          defaultValue={orderOptions[0][1]}
-          value={selectedOrder}
-          onChange={setSelectedOrder}
-        >
-          {orderOptions.map(([value, label]) => (
-            <Option key={value} value={value}>
-              {label}
-            </Option>
-          ))}
-        </Select>
-      </Space>
+      <MonthlyEnergyMixChartControls
+        onChangeStackingMode={onChangeStackingMode}
+        onChangeChartType={onChangeChartType}
+        setSelectedOrder={setSelectedOrder}
+      />
       <HighchartsReact
         allowChartUpdate={true}
         immutable={false}
