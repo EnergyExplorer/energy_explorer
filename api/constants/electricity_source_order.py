f1851f60c9286b1c43daa3d2a1d5a7cf1f9ff7df
kevnflyn <rwestanton@gmail.com>
2022-10-02 15:36:20 +0100

    feat: set specific order for energy sources

---
+++ api/constants/electricity_source_order.py
@@ -0,0 +1,15 @@
+electricity_source_order = [
+  'Electricity|Waste',
+  'Electricity|Biogas',
+  'Electricity|Wood',
+  'Electricity|Hydro RoR',
+  'Electricity|Hydro Dams',
+  'Electricity|Nuclear',
+  'Electricity|Gas',
+  'Electricity|Hydrogen',
+  'Electricity|Geothermal',
+  'Electricity|Wind',
+  'Electricity|Solar',
+  'Electricity|Imports',
+  'Electricity|Total'
+]
