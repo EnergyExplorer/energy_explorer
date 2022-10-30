39ebb69cbd7843f78df3e8fd2f64c13c3f04fa1a
Stéphane Bisinger <stephane@sbisinger.ch>
2022-10-30 15:13:40 +0100

    Fix wrong values for first scenario in comparison

--- src/pages/comparison/ComparisonPageContent.jsx
+++ src/pages/comparison/ComparisonPageContent.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import EnergyOutlookCarousel from "./EnergyOutlookCarousel";
 import FocusScenarioSelect from "./FocusScenarioSelect";
 
 import styles from "./ComparisonPageContent.module.css";
 
 const ComparisonPageContent = ({ slideKey, scenarioData }) => {
-  const [focusScenario, setFocusScenario] = useState(scenarioData[0]);
+  const [focusScenario, setFocusScenario] = useState(
+    JSON.parse(JSON.stringify(scenarioData[0]))
+  );
 
   return (
     <div className={styles.container}>
