69fa897439721efbe7bb2afb92f16438da0a5077
kevnflyn <rwestanton@gmail.com>
2022-11-10 17:24:44 +0100

    style: clean up comments

--- src/pages/home/IntNumberFormat.jsx
+++ src/pages/home/IntNumberFormat.jsx
@@ -1,23 +1,6 @@
 import React from 'react'
 
-// function toPercentage(render, percentage) {
-//   return (value) => render((value * 100).toFixed(0) + "%", percentage(value));
-// }
-
-// function withUnit(unit, render) {
-
-// }
-
 export const IntNumberFormat = ({ value, unit }) => {
   const formatter = new Intl.NumberFormat()
   return `${formatter.format(value)} ${unit}`
 }
-
-
-
-// withUnit(
-//   "M.CHF",
-//   indicate,
-//   getPercentCalculator(minMaxCost),
-//   'large'
-// )
